d9138520b9cc0c6524dcac640875f36dd285e483
Christopher Barnes <cbarnes7404@yahoo.com>
2019-10-28 11:12:41 -0700

    add Save dictionary histogram

--- Code/histogram.py
+++ Code/histogram.py
@@ -1,22 +1,10 @@
 import dictionary_words
 import random
 
-def dictionary_histogram(filename):
-    #histogram = {'one': 1, 'blue': 1, 'two': 1, 'fish': 4, 'red': 1}
-    #sentence = "one fish two fish red fish blue fish"
-    histogram = {}
-
-    all_lines = dictionary_words.get_file_line(filename)
-
-    for index in range(0, len(all_lines)):
-        sentence = all_lines[index]
-        for i in sentence.split():
-            histogram[i] = histogram.get(i, 0) + 1
-
-            most_value = max(histogram, key=histogram.get)
-    print(histogram)
-    save_to_file(histogram, filename + "_histogram.txt")
-    print ("The word with the highest frequency count is: {} with: {} occurrences".format(most_value, histogram[most_value]))
+def save_to_file(histogram_as_dict, filename):
+    with open(filename, 'w+') as f:
+        for key, value in histogram_as_dict.items():
+            f.write("{}\t{}\n".format(key, value))
 
 def find_unique_words(words_list):
     """Record all unique words in a list of strings."""
@@ -42,15 +30,27 @@ def list_histogram(words_list):
         histogram.append([word, appearances])
     return histogram
 
-def save_to_file(histogram_as_dict, filename):
-    with open(filename, 'w+') as f:
-        for key, value in histogram_as_dict.items():
-            f.write("{}\t{}\n".format(key, value))
+def dictionary_histogram(filename):
+    #histogram = {'one': 1, 'blue': 1, 'two': 1, 'fish': 4, 'red': 1}
+    #sentence = "one fish two fish red fish blue fish"
+    histogram = {}
+
+    all_lines = dictionary_words.get_file_line(filename)
+
+    for index in range(0, len(all_lines)):
+        sentence = all_lines[index]
+        for i in sentence.split():
+            histogram[i] = histogram.get(i, 0) + 1
+
+            most_value = max(histogram, key=histogram.get)
+    print(histogram)
+    save_to_file(histogram, filename + "_histogram.txt")
+    print("The word with the highest frequency count is: {} with: {} occurrences".format(
+        most_value, histogram[most_value]))
 
 if __name__ == "__main__":
     #dictionary_histogram("sherlock_homes.txt")
-    dictionary_histogram("Zombie.txt")
+    #dictionary_histogram("Zombie.txt")
 
-    #sentence = "one fish two fish red fish blue fish"
-    #sentence = sentence.split()
-    #print(list_histogram(sentence))
+    sentence = "one fish two fish red fish blue fish"
+    print(list_histogram(sentence.split()))
