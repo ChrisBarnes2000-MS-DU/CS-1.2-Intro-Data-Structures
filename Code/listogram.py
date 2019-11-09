583f224039f53ad216fd12b3f4ff3800240c1720
Christopher Barnes <cbarnes7404@yahoo.com>
2019-11-08 22:15:19 -0800

    add listogram missing one test

--- Code/listogram.py
+++ Code/listogram.py
@@ -21,25 +21,50 @@ class Listogram(list):
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
         # TODO: Increase word frequency by count
+        ind = self.index_of(word)
+        # print("\n", word, "Has an index of: ", ind)
+        if ind is not None:
+            self[ind] = [word, (self.frequency(word) + count)]
+        else:
+            self.append([word, count])
+            self.types += 1
+        self.tokens += count
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
         # TODO: Retrieve word frequency count
+        for ind in self:
+            if ind[0] == word:
+                return ind[1]
+        return 0
 
     def __contains__(self, word):
         """Return boolean indicating if given word is in this histogram."""
         # TODO: Check if word is in this histogram
+        for ind in self:
+            if ind[0] == word:
+                return True
+            else:
+                return False
 
     def index_of(self, target):
         """Return the index of entry containing given target word if found in
         this histogram, or None if target word is not found."""
         # TODO: Implement linear search to find index of entry with target word
+        for word in self:
+            if word[0] == target:
+                return self.index(word)
 
     def sample(self):
         """Return a word from this histogram, randomly sampled by weighting
         each word's probability of being chosen by its observed frequency."""
         # TODO: Randomly choose a word based on its frequency in this histogram
-
+        total = 0
+        ind = random.randint(1, self.tokens)
+        for item in self:
+            total += item[1]
+            if ind <= total:
+                return item[0]
 
 def print_histogram(word_list):
     print()
