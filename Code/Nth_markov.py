6cd335ea5e1fae9c3ef9879b3893d68b6f368b12
Christopher Barnes <cbarnes7404@yahoo.com>
2019-12-07 21:10:45 -0800

    implemented Nth order markov with Que as window, need to fix sampling/sentence gen

--- Code/Nth_markov.py
+++ Code/Nth_markov.py
@@ -4,27 +4,33 @@ from queue import Queue
 from random import sample, choice
 import sys
 
+
 class Nth_Order_Markov():
     def __init__(self, words_list=None, nth_order=2):
         """Construct a Markov Chain model.
            Param: words_list(list of str)"""
         self.corpus = words_list
+        self.total_words = len(self.corpus)
         self.transition = Queue(maxsize=nth_order+2)
         self.chain = self.populate_chain(nth_order)
 
     def populate_chain(self, nth_order):
         self.chain = Dictogram()
-        for i in range(len(self.corpus)-nth_order):
-            # print(i)
-            while not self.transition.empty():
-                self.transition.get()
-            for j in range(nth_order+1):
-                self.transition.put(self.corpus[i+j])
-            # print(self.transition.queue)
-            cur = self.transition.get(), self.transition.queue[0]
-            nxt = self.transition.get(), self.transition.get()
-
-            # print(cur, "\n", nxt)
+        Nth_len = self.total_words-nth_order
+        print("Total words: {}, {} words for Nth_order: {} word pairs".format(self.total_words, Nth_len, nth_order))
+        for i in range(Nth_len):
+            """Look through available words in the corpus relative to the window size"""
+            cur = ""
+            nxt = ""
+            for pos in range(1, nth_order+1):
+                self.transition.put(self.corpus[i+(pos-1)])
+                self.transition.put(self.corpus[i+pos])
+                if pos > 1:
+                    cur += ", "
+                    nxt += ", "
+                cur += self.transition.get()
+                nxt += self.transition.get()
+            print("Current pair: {} -->\t Next pair: {}\n".format(cur, nxt))
             if self.chain.get(cur, None) is None:
                 # print("\n--Adding NEW--")
                 self.chain[cur] = Dictogram([nxt])
@@ -38,13 +44,13 @@ class Nth_Order_Markov():
         '''Takes given word and returns a random word in its markov list'''
         weight_options = self.chain[start_word]
         # print("weight_options: ", weight_options)
-        
+
         total_links = 0
         # for link, count in weight_options.items():
         for count in weight_options.values():
             total_links += count
             # print(link, count)
-        
+
         # print("{} has {} links to go to: {}".format(start_word, total_links, weight_options))
 
         choice = weight_options.sample()
@@ -67,22 +73,25 @@ class Nth_Order_Markov():
         sentence += choice(punctuation)
         return sentence
 
+
 if __name__ == "__main__":
-        #The program only accepts one argument: the number of words to be selected.
-    if len(sys.argv) != 2:
+        # The program only accepts one argument: the number of words to be selected.
+    if len(sys.argv) != 3:
         # print("Try again with 1 argument: the number of words to give!")
         num_words = 10
+        Nth_order = 2
     else:
         #All parameters except the number of words will be hard-coded.
         num_words = int(sys.argv[1])
+        Nth_order = int(sys.argv[2])
 
     # word_list = get_clean_words("text_files/markov.txt")
     word_list = get_clean_words("text_files/second_markov.txt")
     # word_list = get_clean_words("text_files/fish.txt")
     print("\t--word_list--\n", word_list)
 
-    markov = Nth_Order_Markov(word_list, nth_order=2)
-    # print("\n\t--Nth_order_markov--2ND--\n", markov.chain)
-    # print("\n")
+    markov = Nth_Order_Markov(word_list, nth_order=Nth_order)
+    print("\n\t--Nth_order_markov--\n", markov.chain)
+
     sentence = markov.generate_sentence(num_words=num_words)
     print("\n\t--Random Sentence length {}--\n".format(num_words), sentence)
