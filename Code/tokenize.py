34731407fd9edac78b54397cf9d50aa9ffb6d2a5
Christopher Barnes <cbarnes7404@yahoo.com>
2019-12-11 16:46:48 -0800

    add short excerpt for testing, update tokenizer to split by sentence

--- Code/tokenize.py
+++ Code/tokenize.py
@@ -3,10 +3,9 @@ import re
 def tokenize(text):
     print("--Text--\n", text, "\n")
     no_punc_text = remove_punctuation(text)
-    lines = split_on_end(no_punc_text)
-    # tokens = split_on_whitespace(lines)
-    # return tokens
-    return lines
+    sentences = split_by_sentence(no_punc_text)
+    tokens = split_on_whitespace(sentences)
+    return tokens
 
 def remove_punctuation(text):
     no_punc_text = re.sub('[,()]', '', text)
@@ -14,15 +13,24 @@ def remove_punctuation(text):
     print("--No Punctuation--\n", no_punc_text, "\n")
     return no_punc_text
 
-def split_on_end(text):
+def split_by_sentence(text):
     print("--Split by line--")
-    text = re.sub(r'\.', " [STOP].", text)
-    lines = re.split(r'\.', text)
-    [print(line) for line in lines]
-    return lines
+    sentences = re.split(r'\.(?:\s)+', text)
+    # lines = re.sub(r'\.', r'\n', text)
+    for i, sentence in enumerate(sentences):
+        sentences[i] = re.sub(r'^', "[START] ", sentence) + re.sub(r'$', " [STOP]", sentence)
+        print(sentences[i])
+    return sentences
 
-def split_on_whitespace(text):
-    return re.split(r'\s+', text)
+
+
+
+def split_on_whitespace(sentences):
+    tokens = []
+    for sentence in sentences:
+        tokens += re.split(r'\s+', sentence)
+    print("\n--Tokens--\n", tokens)
+    return tokens
 
 if __name__ == '__main__':
     import sys
@@ -30,6 +38,5 @@ if __name__ == '__main__':
         filename = sys.argv[1]
         source = open(filename).read()
         tokens = tokenize(source)
-        # print(tokens)
     else:
         print('No source text filename given as argument')
