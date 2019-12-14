a0052443babf42cb15667283645170aac3bce40e
Christopher Barnes <cbarnes7404@yahoo.com>
2019-12-13 18:09:58 -0800

    adjust route link

--- Code/app.py
+++ Code/app.py
@@ -25,18 +25,18 @@ markov_dict = markov_1.chain(word_list)
 def index():
     return render_template('index.html')
 
-@app.route('Histogram')
+@app.route('Histogram/')
 def Histogram():
     word = histogram.get_word_by_freq(histogram_list)
     histogram_sentence = histogram.make_sentence(histogram_list, num_words)
     return render_template('histo.html', word=word, histogram=histogram_list, histogram_sentence=histogram_sentence, num_words=num_words)
 
-@app.route('First_order')
+@app.route('First_order/')
 def First_order():
     markov_sentence = markov_1.generate_sentence(num_words, markov_dict, word_list)
     return render_template('First_markov.html', word_list=word_list, markov=markov_dict, markov_sentence=markov_sentence)
 
-@app.route('Nth_order')
+@app.route('Nth_order/')
 def Nth_order():
     markov_sentence = nth_markov.generate_sentence(num_words, nth_order)
     return render_template('Nth_markov.html', word_list=word_list, markov=markov_dict, markov_sentence=markov_sentence)
