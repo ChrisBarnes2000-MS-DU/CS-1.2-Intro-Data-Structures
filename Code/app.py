858b3479e60824717267f4e78cbe3a0164660e30
Christopher Barnes <cbarnes7404@yahoo.com>
2019-12-13 16:10:27 -0800

    update app.py

--- Code/app.py
+++ Code/app.py
@@ -1,25 +1,45 @@
 from flask import Flask, render_template, request
-from markov import markov, generate_sentence
-from clean import get_clean_words
+from Nth_markov import Nth_Order_Markov
+from markov import First_Order_Markov
+from utils import get_clean_words
 import histogram
 import os
 
 app = Flask(__name__)
 
+SOURCE = "text_files/markov.txt"
+word_list = get_clean_words(SOURCE)
+num_words = request.args.get("num_words", default=10, type=int)
+nth_order = request.args.get("nth_order", default=2, type=int)
+
+# INITIALIZERS
 histogram = histogram.Histogram()
-histogram_list = histogram.dictionary_histogram("text_files/markov.txt")
-word_list = get_clean_words("text_files/markov.txt")
-markov_dict = markov(word_list)
+markov_1 = First_Order_Markov()
+nth_markov = Nth_Order_Markov(word_list, 2)
+
+# CREATE LISTS
+histogram_list = histogram.dictionary_histogram(SOURCE)
+markov_dict = markov_1.chain(word_list)
 
 @app.route('/')
 def index():
-    num_words = request.args.get("num_words", default=10, type=int)
+    return render_template('index.html')
+
+@app.route('Histogram')
+def Histogram():
     word = histogram.get_word_by_freq(histogram_list)
     histogram_sentence = histogram.make_sentence(histogram_list, num_words)
-    
-    markov_sentence = generate_sentence(num_words, markov_dict, word_list)
-    
-    return render_template('index.html', word_list=word_list, markov=markov_dict, markov_sentence=markov_sentence, word=word, histogram=histogram_list,histogram_sentence=histogram_sentence, num_words=num_words)
+    return render_template('histo.html', word=word, histogram=histogram_list, histogram_sentence=histogram_sentence, num_words=num_words)
+
+@app.route('First_order')
+def First_order():
+    markov_sentence = markov_1.generate_sentence(num_words, markov_dict, word_list)
+    return render_template('First_markov.html', word_list=word_list, markov=markov_dict, markov_sentence=markov_sentence)
+
+@app.route('Nth_order')
+def Nth_order():
+    markov_sentence = nth_markov.generate_sentence(num_words, nth_order)
+    return render_template('Nth_markov.html', word_list=word_list, markov=markov_dict, markov_sentence=markov_sentence)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
